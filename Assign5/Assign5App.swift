87c6ed64d9a943b83c5192738125b4810f9eb8c2
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-04-18 17:26:32 -0400

    Initial Commit

---
+++ Assign5/Assign5App.swift
@@ -0,0 +1,17 @@
+//
+//  Assign5App.swift
+//  Assign5
+//
+//  Created by Nibha Maharjan on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct Assign5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
